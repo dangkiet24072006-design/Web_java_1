4ec062c817419191893433102a84811f76023af3
Kiet Nguyen <dangkiet.24072006@gmail.com>
2026-09-16 14:43:08 +0700

    Kiểm tra giữa kì

---
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+
+
+    List<Student> findByStudentCodeContainingIgnoreCaseOrFullNameContainingIgnoreCaseOrEmailContainingIgnoreCaseOrPhoneContainingIgnoreCase(
+            String studentCode,
+            String fullName,
+            String email,
+            String phone);
+}
